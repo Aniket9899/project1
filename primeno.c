3318387f371b1a7b3ff8d1762572246662a7bb9c
Aniket9899 <111426199+Aniket9899@users.noreply.github.com>
2022-08-17 16:58:17 +0530

    Add files via upload

---
+++ primeno.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ int num1,pr,z;
+ printf("Enter the Number :");
+ scanf("%d",&num1);
+ for(int i=1;i<=num1;i++){
+    if(num1%i==0){
+        z++;
+        //break;
+    }
+
+
+}
+if(z==2){
+        printf("prime number");
+    }
+    else{
+        printf("not prime");
+    }
+
+
+
+ }
